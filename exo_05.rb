36b46e7d7859dab933a43abe676d4ef6a11bee0a
Paul.Graton <paulgtn@hotmail.fr>
2019-01-04 14:00:45 +0100

    Exo 5

---
+++ exo_05.rb
@@ -0,0 +1,25 @@
+ puts "On va compter le nombre d'heures de travail à THP"
+# Donne le nombre d'heures
+ puts "Travail : #{10 * 5 * 11}"
+# Donne le nombre de minutes
+ puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# Question sur les secondes
+puts "Et en secondes ?"
+# Donne le nombre des secondes
+puts 10 * 5 * 11 * 60 * 60
+# Question si valeur de 5 < -2
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# Réponse false
+puts 3 + 2 < 5 - 7 
+# Question additions avec résultats en fin de lignes
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# Remarque suyr le résultat
+puts "OK c'est faux alors"
+# Réquête
+puts "C'est drôle ça, faisons-en plus :"
+# Question si valeur 5 <>= -2 avec résultats en fin de lignes
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 #{5 <= -2}"
+# #{} permet d'isoler l'opération pour qu'elle ne soit pas simplement écrite dans la commande puts sans donner le résultat
