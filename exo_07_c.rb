f90e18cd2cb7f5d2baf7a5bea889149055e1e9c5
Paul.Graton <paulgtn@hotmail.fr>
2019-01-04 14:03:00 +0100

    Exo 7-c

---
+++ exo_07_c.rb
@@ -0,0 +1,3 @@
+user_name = gets.chomp
+puts user_name
+# a Pose question, obtient réponse et répète, b idem mais avec une meilleure ergonomie utilisateur, c juste un écran noir si on ne sait pas ce qu'on doit faire, moins complet que les autres
