0d02b50427a0ad5352c686f8ec45d9646d0116d2
Paul.Graton <paulgtn@hotmail.fr>
2019-01-04 14:01:20 +0100

    Exo 6

---
+++ exo_06.rb
@@ -0,0 +1,8 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+puts "Et en minutes ça fait #{number_of_minutes_in_a_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+# Vu la dernière ajoutée n'a pas de valeur définie, l'opération est impossible
